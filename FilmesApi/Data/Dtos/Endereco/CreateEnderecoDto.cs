bd2ca9f2bfcbee9207094f66e9b19b1c1b1027c1
gabrieltozatto21 <gabriel.tozatto@hotmail.com>
2022-02-04 18:11:25 -0300

    first commit

---
+++ FilmesApi/Data/Dtos/Endereco/CreateEnderecoDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos.Endereco
+{
+    public class CreateEnderecoDto
+    {
+        [Required(ErrorMessage = "O campo de Lagradouro é obrigatório")]
+        public string Lagradouro { get; set; }
+        [Required(ErrorMessage = "O campo de Bairro é obrigatório")]
+        public string Bairro { get; set; }
+        [Required(ErrorMessage = "O campo de Numero é obrigatório")]
+        public int Numero { get; set; }
+    }
+}
---
+++ FilmesApi/Data/Dtos/Endereco/ReadEnderecoDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos.Endereco
+{
+    public class ReadEnderecoDto
+    {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        public string Lagradouro { get; set; }
+        public string Bairro { get; set; }
+        public int Numero { get; set; }
+    }
+}
